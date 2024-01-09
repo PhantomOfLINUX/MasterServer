9ef16e4dd25054d72285ec477f6cb4f5a130b7ac
JeongRae <kkwjdfo@gmail.com>
2024-01-10 06:22:13 +0900

    [Style] 컴포넌트로 변경

--- src/main/java/org/codequistify/master/global/filter/CustomCsrfFilter.java
+++ src/main/java/org/codequistify/master/global/filter/CustomCsrfFilter.java
@@ -4,12 +4,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Configuration
+@Component
 public class CustomCsrfFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
