c9c5862b75002981920395683c66aac830400352
JeongRae <kkwjdfo@gmail.com>
2024-01-28 06:56:40 +0900

    [Feat] test 페이지 home에 등록

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,73 @@
+document
+    .getElementById("emailForm")
+    .addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const email = document.getElementById("email").value;
+        const getUrl = `https://api.pol.or.kr/api/sign-up/email/${email}`;
+
+        fetch(getUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("GET Success:", data);
+            })
+            .catch((error) => {
+                console.error("GET Error:", error);
+            });
+    });
+
+document.getElementById("sendEmail").addEventListener("click", function () {
+    const email = document.getElementById("email").value;
+    const verifyUrl = `https://api.pol.or.kr/api/sign-up/email/${email}/verify`;
+
+    fetch(verifyUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Verification Email Sent:", data);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+});
+
+document.getElementById("getOAuthURL").addEventListener("click", function () {
+    const oauthUrl = "https://api.pol.or.kr:8080/api/oauth2/google-url";
+
+    fetch(oauthUrl, {
+        headers: {
+            Accept: "text/plain",
+        },
+    })
+        .then((response) => response.text())
+        .then((data) => {
+            console.log("OAuth URL:", data);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+});
+
+document
+    .getElementById("loginForm")
+    .addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const email = document.getElementById("loginEmail").value;
+        const password = document.getElementById("loginPassword").value;
+        const loginUrl = "https://api.pol.or.kr/api/sign-in/pol";
+
+        fetch(loginUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email: email, password: password }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Login Success:", data);
+            })
+            .catch((error) => {
+                console.error("Login Error:", error);
+            });
+    });
