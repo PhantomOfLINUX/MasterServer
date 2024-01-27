4cf75712c6527d440f73c8d58192ae3661ecc8a8
JeongRae <kkwjdfo@gmail.com>
2024-01-28 06:56:59 +0900

    [Feat] Exception 핸들러 추가

--- src/main/java/org/codequistify/master/MasterApplication.java
+++ src/main/java/org/codequistify/master/MasterApplication.java
@@ -2,8 +2,12 @@ package org.codequistify.master;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 public class MasterApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/codequistify/master/global/util/BasicResponse.java
+++ src/main/java/org/codequistify/master/global/util/BasicResponse.java
@@ -2,5 +2,7 @@ package org.codequistify.master.global.util;
 
 public record BasicResponse(
         String response,
-        String error) {
+        String error
+)
+{
 }
