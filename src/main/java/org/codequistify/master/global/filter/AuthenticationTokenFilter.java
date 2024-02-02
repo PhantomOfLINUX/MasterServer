2882b2a6986f3fd6b400da4c4bc71776833fe8cb
JeongRae <kkwjdfo@gmail.com>
2024-02-03 06:26:15 +0900

    [Feat] aud 값 servlet에 유지

--- src/main/java/org/codequistify/master/global/filter/AuthenticationTokenFilter.java
+++ src/main/java/org/codequistify/master/global/filter/AuthenticationTokenFilter.java
@@ -1,5 +1,6 @@
 package org.codequistify.master.global.filter;
 
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,11 +9,17 @@ import lombok.RequiredArgsConstructor;
 import org.codequistify.master.global.jwt.TokenProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Component
@@ -23,7 +30,7 @@ public class AuthenticationTokenFilter extends OncePerRequestFilter {
 
     private final ArrayList<String> allowedPaths = new ArrayList<>(List.of(
             "/api/admin/",
-            "/api/quest/"));
+            "/api/stage/"));
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String path = request.getRequestURI();
@@ -36,15 +43,23 @@ public class AuthenticationTokenFilter extends OncePerRequestFilter {
                 break;
             }
         }
-        if (!isAllowed) {
+        if (!isAllowed) { //동록되지 않은 path는 통과
             filterChain.doFilter(request, response);
             return;
         }
 
         String token = tokenProvider.resolveToken(request);
         LOGGER.info("token : {}", token);
-        if (token != null && tokenProvider.isValidatedToken(token)){
-            //
+        Claims claims = tokenProvider.getClaims(token);
+        if (claims != null && tokenProvider.checkExpire(claims)){
+            String aud = claims.getAudience();
+
+            if (aud != null && !aud.isBlank()) {
+                List<GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+                UsernamePasswordAuthenticationToken auth
+                        = new UsernamePasswordAuthenticationToken(aud, null, authorities);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
         }
         else {
             response.setHeader("Authorization", token);
