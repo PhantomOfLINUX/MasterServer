ce48ce1b994389eed6f986b04692903afb10d660
JeongRae <kkwjdfo@gmail.com>
2024-02-05 03:49:28 +0900

    [Feat] userDetails 상속
    Signed-off-by: JeongRae <kkwjdfo@gmail.com>

--- src/main/java/org/codequistify/master/domain/player/service/impl/PlayerDetailsService.java
+++ src/main/java/org/codequistify/master/domain/player/service/impl/PlayerDetailsService.java
@@ -3,21 +3,29 @@ package org.codequistify.master.domain.player.service.impl;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.codequistify.master.domain.player.domain.Player;
-import org.codequistify.master.domain.player.repository.PlayerRepository;
 import org.codequistify.master.domain.player.dto.details.ResetPasswordRequest;
 import org.codequistify.master.domain.player.dto.details.UpdateDetailsRequest;
+import org.codequistify.master.domain.player.repository.PlayerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class PlayerDetailsService {
+public class PlayerDetailsService implements UserDetailsService {
     private final PlayerRepository playerRepository;
 
     private final Logger LOGGER = LoggerFactory.getLogger(PlayerDetailsService.class);
 
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
     @Transactional
     public void resetPassword(ResetPasswordRequest request) {
         Player player = playerRepository.findById(request.id())
@@ -47,4 +55,6 @@ public class PlayerDetailsService {
 
         return player;
     }
+
+
 }
