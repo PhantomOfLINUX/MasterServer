95668bbaa93395e8fa736ad3ddcfad6ea86240b5
JeongRae <kkwjdfo@gmail.com>
2024-03-13 01:14:51 +0900

    [Fix] CORS patch 메서드 허용
    Signed-off-by: JeongRae <kkwjdfo@gmail.com>

--- src/main/java/org/codequistify/master/global/filter/CustomCorsFilter.java
+++ src/main/java/org/codequistify/master/global/filter/CustomCorsFilter.java
@@ -17,7 +17,7 @@ public class CustomCorsFilter extends CorsFilter {
     private static CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://localhost:3000", "http://localhost:8080", "https://www.pol.or.kr", "https://api.pol.or.kr")); // 허용할 출처
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // 허용할 HTTP 메소드
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH")); // 허용할 HTTP 메소드
         configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Accept", "Authorization")); // 허용할 헤더
         configuration.setAllowCredentials(true); // 쿠키 및 인증 정보 허용 설정
         configuration.setMaxAge(3600L); // 사전 요청 캐시 시간
