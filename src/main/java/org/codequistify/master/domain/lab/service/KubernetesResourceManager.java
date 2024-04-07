8d7040d189397ab11a35cc8811b7cf409b278460
JeongRae <kkwjdfo@gmail.com>
2024-04-07 19:17:48 +0900

    [Feat] k8s 리소스 전용 매니저 서비스 분리
    Signed-off-by: JeongRae <kkwjdfo@gmail.com>

---
+++ src/main/java/org/codequistify/master/domain/lab/service/KubernetesResourceManager.java
@@ -0,0 +1,102 @@
+package org.codequistify.master.domain.lab.service;
+
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.StatusDetails;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import lombok.RequiredArgsConstructor;
+import org.codequistify.master.domain.lab.factory.PodFactory;
+import org.codequistify.master.domain.lab.factory.ServiceFactory;
+import org.codequistify.master.domain.lab.utils.KubernetesResourceNaming;
+import org.codequistify.master.domain.stage.domain.Stage;
+import org.codequistify.master.global.aspect.LogExecutionTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+@org.springframework.stereotype.Service
+@RequiredArgsConstructor
+public class KubernetesResourceManager {
+    private final Logger LOGGER = LoggerFactory.getLogger(KubernetesResourceManager.class);
+
+    private final PodFactory podFactory;
+    private final ServiceFactory serviceFactory;
+
+    private final KubernetesClient kubernetesClient;
+
+    @LogExecutionTime
+    public Service createServiceOnKubernetes(Stage stage, String uid) {
+        Service service = serviceFactory.create(stage, 8080, uid);
+
+        service = kubernetesClient.services()
+                .inNamespace("default")
+                .resource(service)
+                .create();
+
+        LOGGER.info("[createServiceOnKubernetes] service: {}", service.getMetadata());
+        return service;
+    }
+
+    @LogExecutionTime
+    public Pod createPodOnKubernetes(Stage stage, String uid) {
+        Pod pod = podFactory.create(stage, 8080, uid);
+
+        pod = kubernetesClient.pods()
+                .inNamespace("default")
+                .resource(pod)
+                .create();
+
+        LOGGER.info("[createPodOnKubernetes] pod: {}", pod.getMetadata());
+        return pod;
+    }
+
+    @LogExecutionTime
+    public void deleteAsyncService(Stage stage, String uid) {
+        String svcName = KubernetesResourceNaming.getServiceName(stage.getStageImage().name(), uid);
+
+        List<StatusDetails> result = kubernetesClient.services()
+                .inNamespace("default")
+                .withName(svcName)
+                .delete();
+
+        LOGGER.info("[deleteServiceIfExists] result {}", result);
+    }
+
+    @LogExecutionTime
+    public void deleteAsyncPod(Stage stage, String uid) {
+        String podName = KubernetesResourceNaming.getPodName(stage.getStageImage().name(), uid);
+
+        List<StatusDetails> result = kubernetesClient.pods()
+                .inNamespace("default")
+                .withName(podName)
+                .delete();
+
+        LOGGER.info("[deletePodIfExists] result {}", result);
+    }
+
+    public Service getService(Stage stage, String uid) {
+        String svcName = KubernetesResourceNaming.getServiceName(stage.getStageImage().name(), uid);
+
+        Service service = kubernetesClient.services()
+                .inNamespace("default")
+                .withName(svcName)
+                .get();
+
+        LOGGER.info("[getServic] name : {}", service.getMetadata().getName());
+        return service;
+    }
+
+    public Pod getPod(Stage stage, String uid) {
+        String podName = KubernetesResourceNaming.getServiceName(stage.getStageImage().name(), uid);
+
+        Pod pod = kubernetesClient.pods()
+                .inNamespace("default")
+                .withName(podName)
+                .get();
+
+        LOGGER.info("[getPod] name : {}", pod.getMetadata().getName());
+        return pod;
+    }
+
+}
--- src/main/java/org/codequistify/master/domain/lab/service/LabService.java
+++ src/main/java/org/codequistify/master/domain/lab/service/LabService.java
@@ -1,16 +1,8 @@
 package org.codequistify.master.domain.lab.service;
 
-import io.fabric8.kubernetes.api.model.Pod;
-import io.fabric8.kubernetes.api.model.Service;
-import io.fabric8.kubernetes.api.model.StatusDetails;
-import io.fabric8.kubernetes.client.KubernetesClient;
 import lombok.RequiredArgsConstructor;
-import org.codequistify.master.domain.lab.factory.PodFactory;
-import org.codequistify.master.domain.lab.factory.ServiceFactory;
-import org.codequistify.master.domain.lab.utils.KubernetesResourceNaming;
 import org.codequistify.master.domain.player.domain.Player;
 import org.codequistify.master.domain.stage.domain.Stage;
-import org.codequistify.master.domain.stage.service.impl.StageServiceImpl;
 import org.codequistify.master.global.aspect.LogExecutionTime;
 import org.codequistify.master.global.exception.ErrorCode;
 import org.codequistify.master.global.exception.domain.BusinessException;
@@ -18,25 +10,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 
-import java.util.List;
-
 @org.springframework.stereotype.Service
 @RequiredArgsConstructor
 public class LabService {
-    private final StageServiceImpl stageService;
+    private final KubernetesResourceManager kubernetesResourceManager;
     private final Logger LOGGER = LoggerFactory.getLogger(LabService.class);
-
-    private final PodFactory podFactory;
-    private final ServiceFactory serviceFactory;
-
-    private final KubernetesClient kubernetesClient;
 
     @LogExecutionTime
     public void createStageOnKubernetes(Player player, Stage stage){
         String uid = player.getUid().toLowerCase();
 
-        this.createServiceOnKubernetes(stage, uid);
-        this.createPodOnKubernetes(stage, uid);
+        kubernetesResourceManager.createServiceOnKubernetes(stage, uid);
+        kubernetesResourceManager.createPodOnKubernetes(stage, uid);
 
         LOGGER.info("[createStageOnKubernetes] stage: {}", stage.getId());
 
@@ -46,76 +31,25 @@ public class LabService {
     public void deleteStageOnKubernetes(Player player, Stage stage) {
         String uid = player.getUid().toLowerCase();
 
-        this.deletePodIfExists(stage, uid);
-        this.deleteServiceIfExists(stage, uid);
+        kubernetesResourceManager.deleteAsyncPod(stage, uid);
+        kubernetesResourceManager.deleteAsyncService(stage, uid);
+    }
+
+    @LogExecutionTime
+    public void deleteSyncStageOnKubernetes(Player player, Stage stage) {
+        String uid = player.getUid().toLowerCase();
+
+        kubernetesResourceManager.deleteAsyncPod(stage, uid);
+        kubernetesResourceManager.deleteAsyncService(stage, uid);
+
     }
 
     @LogExecutionTime
     public boolean existsStageOnKubernetes(Player player, Stage stage) {
-        String podName = KubernetesResourceNaming.getPodName(stage.getStageImage().name(), player.getUid());
-        String svcName = KubernetesResourceNaming.getServiceName(stage.getStageImage().name(), player.getUid());
-
-        boolean podExists = kubernetesClient.pods()
-                .inNamespace("default")
-                .withName(podName)
-                .get() != null;
-
-        boolean serviceExists = kubernetesClient.services()
-                .inNamespace("default")
-                .withName(svcName)
-                .get() != null;
+        boolean podExists = kubernetesResourceManager.getPod(stage, player.getUid()) != null;
+        boolean serviceExists = kubernetesResourceManager.getService(stage, player.getUid()) != null;
 
         return podExists || serviceExists;
-    }
-
-    @LogExecutionTime
-    private Service createServiceOnKubernetes(Stage stage, String uid) {
-        Service service = serviceFactory.create(stage, 8080, uid);
-
-        service = kubernetesClient.services()
-                .inNamespace("default")
-                .resource(service)
-                .create();
-
-        LOGGER.info("[createServiceOnKubernetes] service: {}", service.getMetadata());
-        return service;
-    }
-
-    @LogExecutionTime
-    private Pod createPodOnKubernetes(Stage stage, String uid) {
-        Pod pod = podFactory.create(stage, 8080, uid);
-
-        pod = kubernetesClient.pods()
-                .inNamespace("default")
-                .resource(pod)
-                .create();
-
-        LOGGER.info("[createPodOnKubernetes] pod: {}", pod.getMetadata());
-        return pod;
-    }
-
-    @LogExecutionTime
-    private void deletePodIfExists(Stage stage, String uid) {
-        String podName = KubernetesResourceNaming.getPodName(stage.getStageImage().name(), uid);
-
-        List<StatusDetails> result = kubernetesClient.pods()
-                .inNamespace("default")
-                .withName(podName)
-                .delete();
-
-        LOGGER.info("[deletePodIfExists] result {}", result);
-    }
-
-    @LogExecutionTime
-    private void deleteServiceIfExists(Stage stage, String uid) {
-        String svcName = KubernetesResourceNaming.getServiceName(stage.getStageImage().name(), uid);
-
-        List<StatusDetails> result = kubernetesClient.services()
-                .inNamespace("default")
-                .withName(svcName)
-                .delete();
-
-        LOGGER.info("[deleteServiceIfExists] result {}", result);
     }
 
     public boolean waitForResourceDeletion(Player player, Stage stage) {
