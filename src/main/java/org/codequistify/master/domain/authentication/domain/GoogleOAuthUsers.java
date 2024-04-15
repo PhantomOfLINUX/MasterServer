e3031338c48c7650e8350f01a3d03716fd502099
JeongRae <kkwjdfo@gmail.com>
2024-04-15 18:10:27 +0900

    [Feat] 구글 테이블 추가
    Signed-off-by: JeongRae <kkwjdfo@gmail.com>

--- src/main/java/org/codequistify/master/domain/authentication/domain/GoogleOAuthUsers.java
+++ src/main/java/org/codequistify/master/domain/authentication/domain/GoogleOAuthUsers.java
@@ -2,13 +2,29 @@ package org.codequistify.master.domain.authentication.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+import org.codequistify.master.global.util.BaseTimeEntity;
 
 @ToString
 @Getter
 @Entity
-public class GoogleOAuthUsers {
+public class GoogleOAuthUsers extends BaseTimeEntity {
     @Id
     private String id;
+
+    private String email;
+
+    private String name;
+
+    @Builder
+    public GoogleOAuthUsers(String id, String email, String name) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+    }
+
+    public GoogleOAuthUsers() {
+    }
 }
