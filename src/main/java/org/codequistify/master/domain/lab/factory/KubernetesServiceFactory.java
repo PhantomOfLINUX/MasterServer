2fdb7e1b247dca94347e9c793e5a7f08632a0277
Jeong-Rae <kkwjdfo@gmail.com>
2024-04-22 22:07:53 +0900

    [Feat] pod readiness 추가

--- src/main/java/org/codequistify/master/domain/lab/factory/KubernetesServiceFactory.java
+++ src/main/java/org/codequistify/master/domain/lab/factory/KubernetesServiceFactory.java
@@ -3,16 +3,18 @@ package org.codequistify.master.domain.lab.factory;
 import io.fabric8.kubernetes.api.model.IntOrString;
 import io.fabric8.kubernetes.api.model.Service;
 import io.fabric8.kubernetes.api.model.ServiceBuilder;
+import org.codequistify.master.domain.lab.utils.KubernetesResourceNaming;
 import org.codequistify.master.domain.stage.domain.Stage;
 import org.codequistify.master.domain.stage.domain.StageImageType;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KubernetesServiceFactory implements ServiceFactory{
+    private final static Long ACTIVE_DEADLINE = 10_800L;
     @Override
     public Service create(Stage stage, int port, String uid) {
         StageImageType stageImage = stage.getStageImage();
-        String serviceName = generateServiceName(stageImage, uid);
+        String serviceName = KubernetesResourceNaming.getServiceName(stageImage.name(), uid);
 
         return new ServiceBuilder()
                 .withNewMetadata()
