48cb9a2299d63587d01faae77c463ade0d1b3ef8
Jeong-Rae <kkwjdfo@gmail.com>
2024-05-02 01:49:39 +0900

    [Feat] isComposable 변수 추가
    Signed-off-by: Jeong-Rae <kkwjdfo@gmail.com>

--- src/main/java/org/codequistify/master/domain/stage/repository/QuestionRepository.java
+++ src/main/java/org/codequistify/master/domain/stage/repository/QuestionRepository.java
@@ -2,6 +2,7 @@ package org.codequistify.master.domain.stage.repository;
 
 import org.codequistify.master.domain.stage.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +12,10 @@ public interface QuestionRepository extends JpaRepository<Question, String> {
     boolean existsByStageIdAndIndex(Long stageId, int questionIndex);
 
     Optional<Question> findByStageIdAndIndex(Long stageId, Integer questionIndex);
+
+    @Query("SELECT q.isComposable " +
+            "FROM Question q " +
+            "WHERE q.stage.id = :stageId AND q.index = :questionIndex+1")
+    Boolean isComposableForStageAndIndex(Long stageId, Integer questionIndex);
+
 }
