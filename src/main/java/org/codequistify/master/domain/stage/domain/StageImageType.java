c6513316e75baf67fc3d0a6fd6747799b03f150c
Jeong-Rae <kkwjdfo@gmail.com>
2024-05-06 02:29:29 +0900

    [Feat] 스테이지 등록시 기본이미지 할당
    Signed-off-by: Jeong-Rae <kkwjdfo@gmail.com>

--- src/main/java/org/codequistify/master/domain/stage/domain/StageImageType.java
+++ src/main/java/org/codequistify/master/domain/stage/domain/StageImageType.java
@@ -4,10 +4,14 @@ import lombok.Getter;
 
 @Getter
 public enum StageImageType {
-    S1001("polhub/stage01-ls", "v1"),
-    S1002("polhub/stage02-cd-pwd", "latest"),
-    S1003("jeongrae/socket","latest"),
-    S0000("","");
+    S1001("polhub/s1001", "latest"),
+    S1002("polhub/s1002", "latest"),
+    S1003("polhub/s1003", "latest"),
+    S1004("polhub/s1004", "latest"),
+    S1005("polhub/s1005", "latest"),
+    S1006("polhub/s1006", "latest"),
+    S1007("polhub/s1007", "latest"),
+    S0000("polhub/s1001", "latest");
 
     private final String image;
     private final String tag;
